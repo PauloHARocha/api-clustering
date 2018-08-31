0b6b59bdaf08588087a7326172c1305f303cce78
Paulo Rocha <ph.alcantara.rocha@gmail.com>
2018-08-30 21:13:01 -0300

    Adding cors

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 from flask import Flask
 from flask_restful import Resource, Api
 from results import generate_results
+from flask_cors import CORS
 
 app = Flask(__name__)
 
 api = Api(app)
+CORS(app)
 
 class Results(Resource):
     def get(self, dataset_id, algorithm_id, k):
-        return generate_results(dataset_id, algorithm_id, k)
+        return generate_results(dataset_id, algorithm_id, k)    
 
 api.add_resource(Results, '/<int:dataset_id>/<int:algorithm_id>/<int:k>')
 
