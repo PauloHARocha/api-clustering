0bd3ce69a0cd4a5bb90a40809057bf4662433b31
Paulo Rocha <ph.alcantara.rocha@gmail.com>
2018-09-03 18:43:51 -0300

    Adjusting metrics funcionality.

--- app.py
+++ app.py
@@ -14,13 +14,13 @@ class Iterations(Resource):
         return generate_iterations(dataset_id, algorithm_id, k)
 
 class Comparision(Resource):
-    def get(self, dataset_id, algorithm_id, k, k_min, k_max, n_sim):
-        return generate_comparision(dataset_id, algorithm_id, k, k_min, k_max, n_sim)
+    def get(self, dataset_id, algorithm_id, k_min, k_max, n_sim):
+        return generate_comparision(dataset_id, algorithm_id, k_min, k_max, n_sim)
 
 api.add_resource(Iterations, '/iterations/<int:dataset_id>/<int:algorithm_id>/<int:k>')
 
 api.add_resource(
-    Comparision, '/comparision/<int:dataset_id>/<int:algorithm_id>/<int:k>/<int:k_min>/<int:k_max>/<int:n_sim>')
+    Comparision, '/comparision/<int:dataset_id>/<int:algorithm_id>/<int:k_min>/<int:k_max>/<int:n_sim>')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- comparison.py
+++ comparison.py
@@ -25,7 +25,7 @@ def get_dataset_dimensions(dataset_id):
     return ds.feature_names
 
 
-def generate_comparision(dataset_id, algorithm_id, k, k_min, k_max, n_sim):
+def generate_comparision(dataset_id, algorithm_id, k_min, k_max, n_sim):
     X = get_dataset(dataset_id)
 
     # Normalize the dimension value to a float value with range 0 - 1
@@ -42,28 +42,27 @@ def generate_comparision(dataset_id, algorithm_id, k, k_min, k_max, n_sim):
 def generate_metrics_results(X, algorithm_id, response):
     metrics = ['silhouete', 'calinskiHarabaszIndex', 'gap'] 
     
-    algorithm = get_algorithm(0)
+    algorithm = get_algorithm(algorithm_id)
     algorithm = algorithm(data=X)
-    name = algorithm.__str__()
-
-    response[name] = {}
+    
+    response[algorithm_id] = {}
 
     rng = range(response['k_min'], response['k_max'] + 1)  # 2-16
 
     for k in rng:
-        response[name][k] = {}
+        response[algorithm_id][k] = {}
         for met in metrics:
-            response[name][k][met] = {}
+            response[algorithm_id][k][met] = {}
         for n in range(response['n_sim']):
                     
             algorithm.fit(k=k)
             
             for met in metrics:
                 if met == 'silhouete':
-                    response[name][k][met][n] = Metrics.silhouette(
+                    response[algorithm_id][k][met][n] = Metrics.silhouette(
                         algorithm.clusters, len(X))
                 elif met == 'calinskiHarabaszIndex':
-                    response[name][k][met][n] = Metrics.variance_based_ch(
+                    response[algorithm_id][k][met][n] = Metrics.variance_based_ch(
                         X, algorithm.centroids)
                 elif met == 'gap':
                     clusters = algorithm.clusters
@@ -72,15 +71,15 @@ def generate_metrics_results(X, algorithm_id, response):
                     algorithm.fit(data=random_data)
                     random_clusters = algorithm.clusters
 
-                    response[name][k][met][n] = Metrics.gap_statistic(
+                    response[algorithm_id][k][met][n] = Metrics.gap_statistic(
                         clusters, random_clusters)
 
         for met in metrics:
             met_mean_std = []
-            for n in response[name][k][met]:
-                met_mean_std.append(response[name][k][met][n])
-            response[name][k][met]['mean'] = np.mean(met_mean_std)
-            response[name][k][met]['std'] = np.std(met_mean_std)
+            for n in response[algorithm_id][k][met]:
+                met_mean_std.append(response[algorithm_id][k][met][n])
+            response[algorithm_id][k][met]['mean'] = np.mean(met_mean_std)
+            response[algorithm_id][k][met]['std'] = np.std(met_mean_std)
             
     
     return response
